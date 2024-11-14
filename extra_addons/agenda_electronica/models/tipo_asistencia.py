271b03cf2a6ca12699dd66018b4e2b0baf22a744
Miguel Angel Machuca Yavita <167807941+Miguel-Machuca@users.noreply.github.com>
2024-11-14 15:34:57 -0400

    subiendo modificacion de odoo

--- extra_addons/agenda_electronica/models/tipo_asistencia.py
+++ extra_addons/agenda_electronica/models/tipo_asistencia.py
@@ -4,4 +4,6 @@ class TipoAsistencia(models.Model):
     _name = 'tipo.asistencia'
     _description = 'Modelo de Tipo de Asistencia'
 
+    _rec_name = 'nombre'
+
     nombre = fields.Char(string='Nombre', required=True)
